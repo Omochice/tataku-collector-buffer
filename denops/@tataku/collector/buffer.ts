2c734fab96668b4d8ca804d4709a028a609deeac
Omochice <mochice.mls.ntl@gmail.com>
2024-08-12 01:53:49 +0900

    chore: migrate unknownutil v4

--- denops/@tataku/collector/buffer.ts
+++ denops/@tataku/collector/buffer.ts
@@ -1,14 +1,19 @@
 import { Denops } from "jsr:@denops/std@7.0.3";
 import * as fn from "jsr:@denops/std@7.0.3/function";
-import { assert, is, PredicateType } from "jsr:@core/unknownutil@4.0.3";
+import {
+  as,
+  assert,
+  is,
+  type PredicateType,
+} from "jsr:@core/unknownutil@4.0.3";
 
 const isOption = is.ObjectOf({
   // fn.BufNameArg
-  bufname: is.OptionalOf(is.OneOf([is.Number, is.String])),
+  bufname: as.Optional(is.UnionOf([is.Number, is.String])),
   // fn.BufLnumArg
-  start: is.OptionalOf(is.OneOf([is.Number, is.LiteralOf("$")])),
+  start: as.Optional(is.UnionOf([is.Number, is.LiteralOf("$")])),
   // fn.BufLnumArg
-  end: is.OptionalOf(is.OneOf([is.Number, is.LiteralOf("$")])),
+  end: as.Optional(is.UnionOf([is.Number, is.LiteralOf("$")])),
 });
 
 const defaultOption: Required<PredicateType<typeof isOption>> = {
